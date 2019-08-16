bbd0935231f2caba2f5abcaeb86199734aa60929
Mark Harsley <mharsley45@gmail.com>
2019-08-16 12:06:41 -0400

    string 1 and 2

--- string1.py
+++ string1.py
@@ -24,8 +24,12 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    """Your code goes here.  Edit this docstring."""
-    return
+    if count > 9:
+      amt = 'many'
+    else:
+      amt = str(count)
+  
+    return 'Number of donuts: ' + amt
 
 
 # B. both_ends
@@ -34,9 +38,11 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    """Your code goes here.  Edit this docstring."""
-    return
-
+    if len(s) < 2:
+      return ''
+  
+    return s[0:2] + s[-2:]
+  
 
 # C. fix_start
 # Given a string s, return a string
@@ -48,8 +54,12 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    """Your code goes here.  Edit this docstring."""
-    return
+    char = s[0]
+    length = len(s)
+    s = s.replace(char, '*')
+    s = char + s[1:]
+  
+    return s
 
 
 # D. MixUp
@@ -60,48 +70,49 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    """Your code goes here.  Edit this docstring."""
-    return
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+  
+    return new_a + ' ' + new_b
+  
 
 
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
 def test(got, expected):
-    """Your code goes here.  Edit this docstring."""
     if got == expected:
-        prefix = ' OK '
+      prefix = ' OK '
     else:
-        prefix = '  X '
+      prefix = '  X '
     print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
-    
 
 # Provided main() calls the above functions with interesting inputs,
 # using test() to check if each result is correct or not.
 def main():
-    """Your code goes here.  Edit this docstring."""
-    print('donuts')
+    print ('donuts')
     # Each line calls donuts, compares its result to the expected for that call.
     test(donuts(4), 'Number of donuts: 4')
     test(donuts(9), 'Number of donuts: 9')
     test(donuts(10), 'Number of donuts: many')
     test(donuts(99), 'Number of donuts: many')
-
-    print()
-    print('both_ends')
+  
+    print
+    print ('both_ends')
     test(both_ends('spring'), 'spng')
     test(both_ends('Hello'), 'Helo')
     test(both_ends('a'), '')
     test(both_ends('xyz'), 'xyyz')
-
-    print()
-    print('fix_start')
+  
+  
+    print
+    print ('fix_start')
     test(fix_start('babble'), 'ba**le')
     test(fix_start('aardvark'), 'a*rdv*rk')
     test(fix_start('google'), 'goo*le')
     test(fix_start('donut'), 'donut')
-
-    print()
-    print('mix_up')
+  
+    print
+    print ('mix_up')
     test(mix_up('mix', 'pod'), 'pox mid')
     test(mix_up('dog', 'dinner'), 'dig donner')
     test(mix_up('gnash', 'sport'), 'spash gnort')
